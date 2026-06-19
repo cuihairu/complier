8580b482da547978055c13df3f681177feacfaa8
cuihairu <chuihairu@gmail.com>
2026-06-19 23:20:34 +0800

    feat: add Mermaid diagram support to documentation

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vitepress'
+import { withMermaid } from 'vitepress-plugin-mermaid'
 
-export default defineConfig({
+export default withMermaid(defineConfig({
   // Base path for GitHub Pages
   base: '/oddsmaker/',
 
@@ -93,4 +94,4 @@ export default defineConfig({
       provider: 'local'
     }
   }
-})
+}))
